62a0c43e8b0b6674a2c110001959cef1365ca386
JohelPires <johelpires@gmail.com>
2023-03-09 12:02:07 -0400

    register controller is working

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors')
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
 // Register:
 // - validate name, email, password - with mongoose
@@ -12,9 +12,11 @@ const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
-  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
-    expiresIn: '30d',
-  })
+  const token = user.createJWT()
+  // movendo esse codigo para um method do UserSchema:
+  // const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+  //   expiresIn: '30d',
+  // })
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
